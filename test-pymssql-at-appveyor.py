90ae28974dcbb730ba10c8672fc740458841ec4d
Oleg Broytman <phd@phdru.name>
2018-02-11 20:08:03 +0300

    Create DB, connect, close, drop DB

--- test-pymssql-at-appveyor.py
+++ test-pymssql-at-appveyor.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 
+dbuser = 'sa'
+dbpasswd = 'Password12!'
+dbhost = 'localhost\\SQL2014'
+dbname = 'sqlobject_test'
+
 import pymssql
+conn = pymssql.connect(dbhost, dbuser, dbpasswd, dbname)
+conn.close()
