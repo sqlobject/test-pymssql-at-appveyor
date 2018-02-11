73f940cf51e2bd81a9b9b81a25316948e2f4c9f5
Oleg Broytman <phd@phdru.name>
2018-02-11 21:10:29 +0300

    CRETE TABLE, INSERT, commit

--- test-pymssql-at-appveyor.py
+++ test-pymssql-at-appveyor.py
@@ -7,4 +7,16 @@ dbname = 'sqlobject_test'
 
 import pymssql
 conn = pymssql.connect(dbhost, dbuser, dbpasswd, dbname)
+
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE test (
+        id INT NOT NULL,
+        test VARCHAR(255) NOT NULL,
+        PRIMARY KEY(id)
+    )
+""")
+cursor.execute("INSERT INTO test (id, test) VALUES (1, 'test1')")
+
+conn.commit()
 conn.close()
