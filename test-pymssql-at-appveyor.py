880e80f6e0d2c4e67d85998bb9e6aa718f9e02e8
Oleg Broytman <phd@phdru.name>
2018-02-11 21:41:37 +0300

    Test transactions

--- test-pymssql-at-appveyor.py
+++ test-pymssql-at-appveyor.py
@@ -8,7 +8,9 @@ dbname = 'sqlobject_test'
 import pymssql
 conn = pymssql.connect(dbhost, dbuser, dbpasswd, dbname)
 
+conn.autocommit(True)
 cursor = conn.cursor()
+
 cursor.execute("""
     CREATE TABLE test (
         id INT NOT NULL,
@@ -16,7 +18,19 @@ cursor.execute("""
         PRIMARY KEY(id)
     )
 """)
-cursor.execute("INSERT INTO test (id, test) VALUES (1, 'test1')")
 
+conn.autocommit(False)
+#cursor.execute("BEGIN TRANSACTION")
+
+cursor.execute("INSERT INTO test (id, test) VALUES (1, 'test1')")
+conn.rollback()
+cursor.execute("SELECT * FROM test")
+print(cursor.fetchall())
+
+cursor.execute("INSERT INTO test (id, test) VALUES (1, 'test1')")
 conn.commit()
+cursor.execute("SELECT * FROM test")
+print(cursor.fetchall())
+
+cursor.close()
 conn.close()
