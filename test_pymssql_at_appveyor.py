6f9f42a0bc17bbbf27e8f42cf910eee468358d80
Oleg Broytman <phd@phdru.name>
2018-02-15 01:33:41 +0300

    Copy test_transactions from SQLObject

---
+++ test_pymssql_at_appveyor.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+from sqlobject import SQLObject, StringCol
+from sqlobject.tests import dbtest
+from sqlobject.tests.dbtest import setupClass
+
+
+class PytestOption(object):
+    Database = "mssql://sa:Password12!@localhost\SQL2014/sqlobject_test?driver=pymssql&timeout=30&debug=1"
+    show_sql = True
+    show_sql_output = True
+
+dbtest.conftest.option = PytestOption()
+
+
+class SOTestSOTrans(SQLObject):
+    class sqlmeta:
+        defaultOrder = 'name'
+    name = StringCol(length=10, alternateID=True, dbName='name_col')
+
+
+def test_transaction_commit_sync():
+    setupClass(SOTestSOTrans)
+    connection = SOTestSOTrans._connection
+    trans = connection.transaction()
+    SOTestSOTrans(name='bob')
+
+
+test_transaction_commit_sync()
